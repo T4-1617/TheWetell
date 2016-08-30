7b783397f0745f9bf753f034dc0c829773336f01
Felix Wetell <felix.wetell@gmail.com>
2016-08-30 13:49:17 +0200

    not working code yet, leaving work

--- WindowsFormProgram/WindowsFormProgram/Form1.Designer.cs
+++ WindowsFormProgram/WindowsFormProgram/Form1.Designer.cs
@@ -33,31 +33,34 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Arial", 16F);
-            this.textBox1.Location = new System.Drawing.Point(157, 143);
+            this.textBox1.Location = new System.Drawing.Point(42, 100);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(141, 32);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+       
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Arial", 16F);
-            this.textBox2.Location = new System.Drawing.Point(363, 143);
+            this.textBox2.Location = new System.Drawing.Point(221, 100);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(141, 32);
             this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 16F);
-            this.label1.Location = new System.Drawing.Point(152, 115);
+            this.label1.Location = new System.Drawing.Point(37, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 25);
             this.label1.TabIndex = 2;
@@ -67,7 +70,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 16F);
-            this.label2.Location = new System.Drawing.Point(358, 115);
+            this.label2.Location = new System.Drawing.Point(216, 72);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(118, 25);
             this.label2.TabIndex = 3;
@@ -75,20 +78,57 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.button1.Font = new System.Drawing.Font("Arial", 16F);
-            this.button1.Location = new System.Drawing.Point(246, 181);
+            this.button1.Location = new System.Drawing.Point(123, 138);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 71);
             this.button1.TabIndex = 4;
             this.button1.Text = "Hello!";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 16F);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(22, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(367, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please enter your first and last name:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 10F);
+            this.button2.Location = new System.Drawing.Point(163, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F);
+            this.label4.Location = new System.Drawing.Point(128, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Save names to a file:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 391);
+            this.ClientSize = new System.Drawing.Size(415, 344);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -108,6 +148,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormProgram/WindowsFormProgram/Form1.cs
+++ WindowsFormProgram/WindowsFormProgram/Form1.cs
@@ -17,19 +17,25 @@ namespace WindowsFormProgram
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello " + textBox1.Text + " " + textBox2.Text + "!");
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonSave()
+        {
+            
         }
     }
 }
