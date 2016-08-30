3a87fcfc11e5700e8e20d84f8d58c18594356a25
Felix Wetell <felix.wetell@gmail.com>
2016-08-30 13:11:52 +0200

    added method "force name" to program

--- ConsoleNameProgram/ConsoleNameProgram/Program.cs
+++ ConsoleNameProgram/ConsoleNameProgram/Program.cs
@@ -8,14 +8,38 @@ namespace ConsoleNameProgram
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("What´s your first name?");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("What´s your last name?");
-            string lastName = Console.ReadLine();
 
-            Console.WriteLine("Hello " + lastName + "!");
+        static void Main( string[] args)
+        {
+
+            string firstName = "";
+            string lastName = "";
+
+            Console.WriteLine("What´s your first name?");
+            firstName = ReadName();
+
+            Console.WriteLine("What´s your last name?");
+            lastName = ReadName();
+
+            Console.WriteLine("Hello "+ lastName + "!");
+
+        }
+
+        private static string ReadName()
+        {
+            string name;
+
+            while (true)
+            {
+                name = Console.ReadLine();
+                if (name != "")
+                {
+                    break;
+                }
+                Console.WriteLine("Please enter a name:");
+            }
+
+            return name;
         }
     }
 }
