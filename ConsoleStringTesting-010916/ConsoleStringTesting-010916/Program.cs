6fe4420bc5f067cc863df9f6b6862508cf194335
Felix Wetell <felix.wetell@gmail.com>
2016-09-01 12:42:32 +0200

    Working code until point 2.7 - checkpoint

--- ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
+++ ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleStringTesting_010916
+namespace ConsoleNameProgram_310916
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string name;
+
+            Console.WriteLine("Enter your firstname and lastname");
+            name = "Kalle Anka";
+
+            Console.WriteLine(name.Length);
+
+            Console.WriteLine(name.Replace(" ", string.Empty).Length);
+
+            string firstName;
+            firstName = name.Split(' ').First();
+            Console.WriteLine(firstName);
+
+            string lastName;
+            lastName = name.Split(' ').Last();
+            Console.WriteLine(lastName);
+
+            string reverseNames = name;
+            reverseNames = string.Format("{0}, {1}", lastName, firstName);
+            Console.WriteLine(reverseNames);
+
+            string padFirstName = firstName.ToUpper();
+            foreach (char item in padFirstName)
+            {
+                Console.WriteLine("----->" + item);
+            }
+
+            string padLastName = lastName.ToUpper();
+            foreach (char item in padLastName)
+            {
+                Console.WriteLine("------>" + item);
+            }
+
+
+
         }
     }
 }
