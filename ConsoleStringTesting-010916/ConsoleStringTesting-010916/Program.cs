ff2dd658cf56bd411ae869f2a980cd5d52136cd5
Felix Wetell <felix.wetell@gmail.com>
2016-09-01 13:57:31 +0200

    Fully commented code - checkpoint (ConsoleStringTesting - 010916)

--- ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
+++ ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
@@ -13,24 +13,30 @@ namespace ConsoleNameProgram_310916
             string name;
 
             Console.WriteLine("Enter your firstname and lastname");
-            name = "Kalle Anka";
+            name = Console.ReadLine();
 
+            //Prints the length of the string
             Console.WriteLine(name.Length);
 
+            //Same thing as above, but removes the white space before counting the length
             Console.WriteLine(name.Replace(" ", string.Empty).Length);
 
+            //Removes everything from the string after the first whitespace 
             string firstName;
             firstName = name.Split(' ').First();
             Console.WriteLine(firstName);
 
+            //Removes everything before the last white space
             string lastName;
             lastName = name.Split(' ').Last();
             Console.WriteLine(lastName);
 
+            //Takes the last name and puts it first also putting the first name last  
             string reverseNames = name;
             reverseNames = string.Format("{0}, {1}", lastName, firstName);
             Console.WriteLine(reverseNames);
 
+            //Adds padding and splits the string into different lines
             string padFirstName = firstName.ToUpper();
             foreach (char item in padFirstName)
             {
@@ -43,15 +49,19 @@ namespace ConsoleNameProgram_310916
                 Console.WriteLine("------>" + item);
             }
 
+            //Uses method "ReverseNames"
             string lowercaseNames = name.ToLower();
             Console.WriteLine(ReverseNames(lowercaseNames));
 
+            //Uses method "UpperLowerCase"
             Console.WriteLine(UpperLowerCase(name));
 
+            //Uses method "LeetSpeech"
             Console.WriteLine(LeetSpeech(name));
 
         }
 
+        //Method that reverse the string that is sent in
         private static string ReverseNames (string str)
         {
             char[] reverseNames = str.ToCharArray();
@@ -59,6 +69,7 @@ namespace ConsoleNameProgram_310916
             return new string(reverseNames);
         }
 
+        //Method that takes a normal string and makes every other letter uppercase
         private static string UpperLowerCase (string str)
         {
             char[] upperLowerCase = str.ToCharArray();
@@ -73,6 +84,7 @@ namespace ConsoleNameProgram_310916
             return new string(upperLowerCase);
         }
 
+        //Mothod that converts a normal string of text to "leet" text
         private static string LeetSpeech(string str)
         {
             char[] leetSpeech = str.ToCharArray();
