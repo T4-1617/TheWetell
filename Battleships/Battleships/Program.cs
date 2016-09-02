884702efd6095bb358fb3d62db0def77e4feaefb
Felix Wetell <felix.wetell@gmail.com>
2016-09-02 14:20:00 +0200

    Separeted CreateMap method to several methods - Battleships (020916)

--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -10,16 +10,20 @@ namespace Battleships
     {
         static void Main(string[] args)
         {
-            string map = string.Empty;
+            int boats = r.Next(3, 6);
+
             CreateGameMap();
-
-
+            PlaceBoats(boats);
+            DrawGameMap();
 
         }
 
+        static bool[,] gameMap = new bool[7, 5];
+        static Random r = new Random();
+
+        //Creates the gamemap
         private static void CreateGameMap()
         {
-            bool[,] gameMap = new bool[7, 5];
 
             for (int x = 0; x < 7; x++)
             {
@@ -28,7 +32,27 @@ namespace Battleships
                     gameMap[x, y] = false;
                 }
             }
+        }
+        //Creates boats
+        private static void PlaceBoats(int boatsToPlace)
+        {
 
+            int newBoats = 0;
+            while (newBoats < boatsToPlace)
+            {
+                int x = r.Next(0, 7);
+                int y = r.Next(0, 5);
+
+                if (gameMap[x, y] == false)
+                {
+                    gameMap[x, y] = true;
+                    newBoats++;
+                }
+            }
+        }
+        //Draw gamemap
+        private static void DrawGameMap()
+        {
             string draw = string.Empty;
             for (int x = 0; x < 7; x++)
             {
@@ -45,9 +69,7 @@ namespace Battleships
                     Console.Write(draw);
                 }
                 Console.WriteLine();
-
             }
-
         }
 
     }
