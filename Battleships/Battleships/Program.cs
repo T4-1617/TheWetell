d40d19102c5478316b2645a34a95931263568cd1
Felix Wetell <felix.wetell@gmail.com>
2016-09-02 12:43:06 +0200

    Gamemap sort of working, just one little bug - Battleships (020916)

--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -10,6 +10,46 @@ namespace Battleships
     {
         static void Main(string[] args)
         {
+            string map = string.Empty;
+            Console.WriteLine(CreateGameMap());
+
+
+
         }
+
+        private static string CreateGameMap()
+        {
+            bool[,] gameMap = new bool[7, 5];
+
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    gameMap[x, y] = false;
+                }
+            }
+
+            string draw = string.Empty;
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (gameMap[x, y] == true)
+                    {
+                        draw = " X ";
+                    }
+                    else
+                    {
+                        draw = " O ";
+                    }
+                    Console.Write(draw);
+                }
+                Console.WriteLine();
+
+            }
+            return draw;
+
+        }
+
     }
 }
--- ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
+++ ConsoleStringTesting-010916/ConsoleStringTesting-010916/Program.cs
@@ -110,4 +110,3 @@ namespace ConsoleNameProgram_310916
         }
     }
 }
-
