cda38022c2a2ef0c73e5a89208dc463c6cf79610
Felix Wetell <felix.wetell@gmail.com>
2016-09-05 14:09:13 +0200

    Initial commit - Checkpoint (ConsoleCardGame - 050916)

---
+++ ConsoleCardGame - 050916/ConsoleCardGame - 050916/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCardGame___050916
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
