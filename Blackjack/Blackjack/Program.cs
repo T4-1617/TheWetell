bf7ab1af57c47d45fc7ce1ed0f3c2a85f74a98d4
Felix Wetell <felix.wetell@gmail.com>
2016-09-07 09:41:39 +0200

    Added class for creating cards - Checkpoint 070916 (Blackjack)

--- Blackjack/Blackjack/Program.cs
+++ Blackjack/Blackjack/Program.cs
@@ -8,8 +8,25 @@ namespace Blackjack
 {
     class Program
     {
+        static List<Card> doubleDeck = new List<Card>();
+
         static void Main(string[] args)
         {
+
+        }
+
+        // Templete for creating individual cards
+        class Card
+        {
+            public int colour;
+            public int number;
+
+            public Card(int value, int num)
+            {
+                colour = value;
+                number = num;
+            }
+
         }
     }
 }
