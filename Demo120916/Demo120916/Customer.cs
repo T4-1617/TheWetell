66e5e68c13319c4d557e5244283b4a6260eaa357
Felix Wetell <felix.wetell@gmail.com>
2016-09-12 11:30:27 +0200

    Random commit

---
+++ Demo120916/Demo120916/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo120916
+{
+    class Customer
+    {
+        public string FirstName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+            
+        }
+        public string LastName { get; set; }
+    }
+}
---
+++ Demo120916/Demo120916/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo120916
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car a = new Car();
+            a.Colour = "Blue";
+            Console.WriteLine(a.Colour);
+        }
+    }
+
+    public class Car
+    {
+        public string RegNumber { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+    }
+}
