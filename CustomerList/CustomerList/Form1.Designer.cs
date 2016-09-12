beb431028b83b559f273099e61b54d22aeb47c30
Felix Wetell <felix.wetell@gmail.com>
2016-09-12 13:17:30 +0200

    [CustomerList] Complete code

--- CustomerList/CustomerList/Form1.Designer.cs
+++ CustomerList/CustomerList/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CustomerList
 {
-    partial class Form1
+    partial class Customers
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnAddCustomer = new System.Windows.Forms.Button();
+            this.listBoxCustomers = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumberCustomers = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Arial", 14F);
+            this.txtFirstName.Location = new System.Drawing.Point(145, 12);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(173, 29);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Arial", 14F);
+            this.txtLastName.Location = new System.Drawing.Point(145, 47);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(173, 29);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // btnAddCustomer
+            // 
+            this.btnAddCustomer.Font = new System.Drawing.Font("Arial", 16F);
+            this.btnAddCustomer.Location = new System.Drawing.Point(278, 88);
+            this.btnAddCustomer.Name = "btnAddCustomer";
+            this.btnAddCustomer.Size = new System.Drawing.Size(40, 31);
+            this.btnAddCustomer.TabIndex = 2;
+            this.btnAddCustomer.Text = "+";
+            this.btnAddCustomer.UseVisualStyleBackColor = true;
+            this.btnAddCustomer.Click += new System.EventHandler(this.btnAddCustomer_Click);
+            // 
+            // listBoxCustomers
+            // 
+            this.listBoxCustomers.FormattingEnabled = true;
+            this.listBoxCustomers.Location = new System.Drawing.Point(16, 125);
+            this.listBoxCustomers.Name = "listBoxCustomers";
+            this.listBoxCustomers.Size = new System.Drawing.Size(302, 95);
+            this.listBoxCustomers.TabIndex = 3;
+            this.listBoxCustomers.SelectedIndexChanged += new System.EventHandler(this.listBoxCustomers_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 22);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "You have";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F);
+            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 14F);
+            this.label3.Location = new System.Drawing.Point(12, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Last name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 14F);
+            this.label4.Location = new System.Drawing.Point(135, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "number of customers.";
+            // 
+            // txtNumberCustomers
+            // 
+            this.txtNumberCustomers.Font = new System.Drawing.Font("Arial", 14F);
+            this.txtNumberCustomers.Location = new System.Drawing.Point(104, 237);
+            this.txtNumberCustomers.Name = "txtNumberCustomers";
+            this.txtNumberCustomers.Size = new System.Drawing.Size(25, 29);
+            this.txtNumberCustomers.TabIndex = 8;
+            // 
+            // Customers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(348, 283);
+            this.Controls.Add(this.txtNumberCustomers);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxCustomers);
+            this.Controls.Add(this.btnAddCustomer);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Customers";
+            this.Text = "Customers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnAddCustomer;
+        private System.Windows.Forms.ListBox listBoxCustomers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNumberCustomers;
     }
 }
 
--- CustomerList/CustomerList/Form1.cs
+++ CustomerList/CustomerList/Form1.cs
@@ -10,11 +10,50 @@ using System.Windows.Forms;
 
 namespace CustomerList
 {
-    public partial class Form1 : Form
+    public partial class Customers : Form
     {
-        public Form1()
+        string status;
+        int i = 100;
+
+        public Customers()
         {
             InitializeComponent();
+        }
+
+        List<Customer> customers = new List<Customer>();
+
+        private void btnAddCustomer_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer();
+
+            customer.ID = i;
+            customer.FirstName = txtFirstName.Text;
+            customer.LastName = txtLastName.Text;
+            customer.Active = true;
+
+            customers.Add(customer);
+
+            if (customer.Active)
+            {
+                status = "Active";
+            }
+            else
+            {
+                status = "Unactive";
+            }
+
+            txtNumberCustomers.Text = string.Format("{0}", customers.Count);
+            listBoxCustomers.Items.Add(i + ". " + customer.FullName() + " - " + "Status: " + status);
+
+            i++;
+            txtFirstName.Focus();
+        }
+
+        private void listBoxCustomers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Customer c = customers[listBoxCustomers.SelectedIndex];
+            MessageBox.Show(string.Format("ID: {0}\nFist name: {1}\nLast name: {2}\nStatus: {3}", 
+                c.ID, c.FirstName, c.LastName, c.Active), "Customer");
         }
     }
 }
