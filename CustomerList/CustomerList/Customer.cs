92223e13394853601867370211cfd27b2a652892
Felix Wetell <felix.wetell@gmail.com>
2016-09-12 13:17:14 +0200

    [CustomerList] Added class "Customers"

---
+++ CustomerList/CustomerList/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerList
+{
+    class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName()
+        {
+            return string.Format("{0} {1}", 
+                FirstName, LastName);
+        }
+        public bool Active { get; set; }
+    }
+}
