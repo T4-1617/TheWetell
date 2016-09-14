e4ed2756960cb8b57d68f50af7e06e2d39b283af
Felix Wetell <felix.wetell@gmail.com>
2016-09-14 12:02:30 +0200

    [CarCompany] Created class for "cars"

---
+++ CarCompany/CarCompany/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCompany
+{
+    class Car
+    {
+        Random r = new Random();
+        public int RegNumber()
+        {
+            int plate = r.Next(100, 500);
+            return plate;
+        }
+        public string Type { get; set; }
+        public int Seats { get; set; }
+        public int Volume { get; set; }
+    }
+}
