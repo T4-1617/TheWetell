db3f6f29b054aad8810d041fb06ff703fc7bb56b
Felix Wetell <felix.wetell@gmail.com>
2016-09-15 11:48:18 +0200

    [CarCompany] Added a method and cleaned up some the code

--- CarCompany/CarCompany/Form1.cs
+++ CarCompany/CarCompany/Form1.cs
@@ -20,13 +20,12 @@ namespace CarCompany
 
             panelInfo.Visible = false;
 
-            txtMake.ReadOnly = true;
-            txtModel.ReadOnly = true;
-            txtRegNum.ReadOnly = true;
-            txtSeats.ReadOnly = true;
+            // Sets all textboxes to non-writable
+            editInfo(true);
 
             listBoxCars.DisplayMember = "make";
 
+            // Sets a random registration number on each vehicle
             Random r = new Random();
             listBoxCars.Items.Add(new Car() { make = "Volvo", regNumber = r.Next(100,500) });
             listBoxCars.Items.Add(new Car() { make = "Ford", regNumber = r.Next(100, 500) });
@@ -40,6 +39,7 @@ namespace CarCompany
         {
             Car car = (Car)listBoxCars.SelectedItem;
 
+            // Sets textboxes to car values
             txtMake.Text = car.make;
             txtModel.Text = car.model;
             txtRegNum.Text = car.regNumber.ToString();
@@ -50,19 +50,21 @@ namespace CarCompany
 
         private void btnEdit_Click(object sender, EventArgs e)
         {
-            txtModel.ReadOnly = false;
-            txtSeats.ReadOnly = false;
+            // Sets textboxes to writable
+            editInfo(false);
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
             Car car = (Car)listBoxCars.SelectedItem;
 
+            // Lets user change the variable values
             car.model = txtModel.Text;
             car.seats = txtSeats.Text;
 
-            txtModel.ReadOnly = true;
-            txtSeats.ReadOnly = true;
+            editInfo(true);
+
+            // Sleep used to make the program less rigid
             Thread.Sleep(200);
             panelInfo.Visible = false;
         }
@@ -71,6 +73,29 @@ namespace CarCompany
         {
             Car car = (Car)listBoxCars.SelectedItem;
             panelInfo.Visible = false;
+        }
+
+        // Method that sets text boxes to writable or not
+        private void editInfo(bool edit)
+        {
+            switch (edit)
+            {
+                // Textboxes become non-writable
+                case true:
+                    txtMake.ReadOnly = true;
+                    txtModel.ReadOnly = true;
+                    txtRegNum.ReadOnly = true;
+                    txtSeats.ReadOnly = true;
+                    break;
+                
+                // Some textboxes become writable
+                case false:
+                    txtModel.ReadOnly = false;
+                    txtSeats.ReadOnly = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
