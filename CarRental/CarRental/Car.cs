b026fb157ebeaa28f095133c404d22ad1bf1000d
Felix Wetell <felix.wetell@gmail.com>
2016-09-16 09:53:36 +0200

    [CarRental] Added values set values to Car class

--- CarRental/CarRental/Car.cs
+++ CarRental/CarRental/Car.cs
@@ -8,6 +8,17 @@ namespace CarRental
 {
     class Car
     {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int RegNumber { get; set; }
+        public bool Hired { get; set; }
 
+        public Car()
+        {
+            Make = "Unkown";
+            Model = "X";
+            RegNumber = 0;
+            Hired = false;
+        }
     }
 }
