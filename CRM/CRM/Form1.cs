e9226fafc8b55d3173ace621a5f54edefb2e365e
Felix Wetell <felix.wetell@gmail.com>
2016-09-21 14:56:26 +0200

    [CRM] Save button for edit panel adn working code!

--- CRM/CRM/Form1.cs
+++ CRM/CRM/Form1.cs
@@ -114,11 +114,35 @@ namespace CRM
             }
         }
 
-        // Save button for edit panel 
+        // Save button for edit panel
+        private Customer customer; 
+        private Employee employee;
+        private Supplier supplier;
         private void btnSaveEdit_Click(object sender, EventArgs e)
         {
-            CheckPeople();
             ClearText("edit");
+            Person person = (Person)lbPersonList.SelectedItem;
+
+            person.FirstName = txtFirstNameEdit.Text;
+            person.LastName = txtLastNameEdit.Text;
+            person.PhoneNumber = txtPhoneNumberEdit.Text;
+
+            switch (person.GetType().Name)
+            {
+                case "Employee":
+                    employee.Salary = txtSalaryEdit.Text;
+                    employee.Title = txtTitleEdit.Text;
+                    break;
+
+                case "Supplier":
+                    supplier.Company = txtCompanyEdit.Text;
+                    break;
+
+                default:
+                    break;
+            }
+
+            CheckPeople();
         }
         private void txtPhoneNumberEdit_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -152,21 +176,36 @@ namespace CRM
 
         private void lbPersonList_SelectedIndexChanged(object sender, EventArgs e)
         {
+            ClearText("edit");
             pnlEditInfo.Visible = true;
             Person person = (Person)lbPersonList.SelectedItem;
+
+            txtFirstNameEdit.Text = person.FirstName;
+            txtLastNameEdit.Text = person.LastName;
+            txtPhoneNumberEdit.Text = person.PhoneNumber;
 
             switch (person.GetType().Name)
             {
                 case "Customer":
                     GetPersonType(4);
+                    
                     break;
 
                 case "Employee":
                     GetPersonType(5);
+
+                    Employee employee = (Employee)lbPersonList.SelectedItem;
+
+                    txtSalaryEdit.Text = employee.Salary;
+                    txtTitleEdit.Text = employee.Title;
                     break;
 
                 case "Supplier":
                     GetPersonType(6);
+
+                    Supplier supplier = (Supplier)lbPersonList.SelectedItem;
+
+                    txtCompanyEdit.Text = supplier.Company;
                     break;
 
                 default:
@@ -188,47 +227,58 @@ namespace CRM
             txtLastNameEdit.ReadOnly = false;
             txtPhoneNumber.ReadOnly = false;
             txtPhoneNumberEdit.ReadOnly = false;
-
-            switch (value)
+            if (value <= 3)
             {
-                // Case when choosing "Customer"
-                case 1:
-                    pnlEnterInfo.Visible = true;
-                    break;
+                switch (value)
+                {
+                    // Case when choosing "Customer"
+                    case 1:
+                        pnlEnterInfo.Visible = true;
+                        break;
 
-                // Case when choosing "Employee"
-                case 2:
-                    pnlEnterInfo.Visible = true;
+                    // Case when choosing "Employee"
+                    case 2:
+                        pnlEnterInfo.Visible = true;
 
-                    txtTitle.ReadOnly = false;
-                    txtSalary.ReadOnly = false;
-                    break;
+                        txtTitle.ReadOnly = false;
+                        txtSalary.ReadOnly = false;
+                        break;
 
-                //Case when choosing "Supplier" 
-                case 3:
-                    pnlEnterInfo.Visible = true;
+                    //Case when choosing "Supplier" 
+                    case 3:
+                        pnlEnterInfo.Visible = true;
 
-                    txtCompany.ReadOnly = false;
-                    break;
+                        txtCompany.ReadOnly = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-                case 4:
-                    pnlEditInfo.Visible = true;
-                    break;
+            else
+            {
+                switch (value)
+                {
+                    case 4:
+                        pnlEditInfo.Visible = true;
+                        break;
 
-                case 5:
-                    pnlEditInfo.Visible = true;
-                    txtTitleEdit.ReadOnly = false;
-                    txtSalaryEdit.ReadOnly = false;
-                    break;
+                    case 5:
+                        pnlEditInfo.Visible = true;
 
-                case 6:
-                    pnlEditInfo.Visible = true;
+                        txtTitleEdit.ReadOnly = false;
+                        txtSalaryEdit.ReadOnly = false;
+                        break;
 
-                    txtCompanyEdit.ReadOnly = false;
-                    break;
+                    case 6:
+                        pnlEditInfo.Visible = true;
 
-                default:
-                    break;
+                        txtCompanyEdit.ReadOnly = false;
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
 
@@ -298,7 +348,6 @@ namespace CRM
                     txtCompany.Clear();
                     txtTitle.Clear();
 
-                    GetPersonType(0);
                     break;
 
                 case "edit":
@@ -309,7 +358,6 @@ namespace CRM
                     txtCompanyEdit.Clear();
                     txtTitleEdit.Clear();
 
-                    GetPersonType(0);
                     break;
                 default:
                     break;
