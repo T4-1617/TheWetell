57a02096d7a4b974cb0b63a7255ec2e9490e12c8
Felix Wetell <felix.wetell@gmail.com>
2016-09-22 14:49:49 +0200

    [OnlineBooking] Added static users with accounts and made it posible to see accounts for each user

--- OnlineBooking/OnlineBooking/Form1.Designer.cs
+++ OnlineBooking/OnlineBooking/Form1.Designer.cs
@@ -31,18 +31,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.cbxSelectUser = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.tbxAddAccount = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddAccount = new System.Windows.Forms.Button();
             this.pnlCustomerButtons = new System.Windows.Forms.Panel();
             this.btnWithdrawCash = new System.Windows.Forms.Button();
-            this.btnAddCash = new System.Windows.Forms.Button();
             this.btnBalance = new System.Windows.Forms.Button();
+            this.btnAddCash = new System.Windows.Forms.Button();
             this.btnOpenAccount = new System.Windows.Forms.Button();
             this.btnInvolement = new System.Windows.Forms.Button();
             this.lbxUsers = new System.Windows.Forms.ListBox();
             this.lbxUserAccounts = new System.Windows.Forms.ListBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.btnAddAccount = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.pnlCustomerButtons.SuspendLayout();
             this.SuspendLayout();
@@ -51,7 +55,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 16F);
-            this.label1.Location = new System.Drawing.Point(381, 43);
+            this.label1.Location = new System.Drawing.Point(372, 48);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 25);
             this.label1.TabIndex = 0;
@@ -59,126 +63,32 @@
             // 
             // cbxSelectUser
             // 
+            this.cbxSelectUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxSelectUser.Font = new System.Drawing.Font("Arial", 12F);
             this.cbxSelectUser.FormattingEnabled = true;
-            this.cbxSelectUser.Location = new System.Drawing.Point(295, 71);
+            this.cbxSelectUser.Location = new System.Drawing.Point(295, 76);
             this.cbxSelectUser.Name = "cbxSelectUser";
-            this.cbxSelectUser.Size = new System.Drawing.Size(296, 26);
+            this.cbxSelectUser.Size = new System.Drawing.Size(278, 26);
             this.cbxSelectUser.TabIndex = 1;
+            this.cbxSelectUser.SelectedIndexChanged += new System.EventHandler(this.cbxSelectUser_SelectedIndexChanged);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.tbxAddAccount);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.btnAddAccount);
-            this.panel1.Location = new System.Drawing.Point(254, 449);
+            this.panel1.Location = new System.Drawing.Point(844, 126);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(379, 220);
             this.panel1.TabIndex = 2;
             // 
-            // pnlCustomerButtons
+            // tbxAddAccount
             // 
-            this.pnlCustomerButtons.Controls.Add(this.btnWithdrawCash);
-            this.pnlCustomerButtons.Controls.Add(this.btnAddCash);
-            this.pnlCustomerButtons.Controls.Add(this.btnBalance);
-            this.pnlCustomerButtons.Location = new System.Drawing.Point(12, 300);
-            this.pnlCustomerButtons.Name = "pnlCustomerButtons";
-            this.pnlCustomerButtons.Size = new System.Drawing.Size(99, 220);
-            this.pnlCustomerButtons.TabIndex = 3;
-            // 
-            // btnWithdrawCash
-            // 
-            this.btnWithdrawCash.Font = new System.Drawing.Font("Arial", 10F);
-            this.btnWithdrawCash.Location = new System.Drawing.Point(3, 149);
-            this.btnWithdrawCash.Name = "btnWithdrawCash";
-            this.btnWithdrawCash.Size = new System.Drawing.Size(92, 67);
-            this.btnWithdrawCash.TabIndex = 6;
-            this.btnWithdrawCash.Text = "Withdraw cash";
-            this.btnWithdrawCash.UseVisualStyleBackColor = true;
-            // 
-            // btnAddCash
-            // 
-            this.btnAddCash.Font = new System.Drawing.Font("Arial", 10F);
-            this.btnAddCash.Location = new System.Drawing.Point(3, 76);
-            this.btnAddCash.Name = "btnAddCash";
-            this.btnAddCash.Size = new System.Drawing.Size(92, 67);
-            this.btnAddCash.TabIndex = 5;
-            this.btnAddCash.Text = "Add cash";
-            this.btnAddCash.UseVisualStyleBackColor = true;
-            // 
-            // btnBalance
-            // 
-            this.btnBalance.Font = new System.Drawing.Font("Arial", 10F);
-            this.btnBalance.Location = new System.Drawing.Point(3, 3);
-            this.btnBalance.Name = "btnBalance";
-            this.btnBalance.Size = new System.Drawing.Size(92, 67);
-            this.btnBalance.TabIndex = 4;
-            this.btnBalance.Text = "Balance";
-            this.btnBalance.UseVisualStyleBackColor = true;
-            // 
-            // btnOpenAccount
-            // 
-            this.btnOpenAccount.Font = new System.Drawing.Font("Arial", 10F);
-            this.btnOpenAccount.Location = new System.Drawing.Point(15, 230);
-            this.btnOpenAccount.Name = "btnOpenAccount";
-            this.btnOpenAccount.Size = new System.Drawing.Size(92, 67);
-            this.btnOpenAccount.TabIndex = 7;
-            this.btnOpenAccount.Text = "Open account";
-            this.btnOpenAccount.UseVisualStyleBackColor = true;
-            // 
-            // btnInvolement
-            // 
-            this.btnInvolement.Font = new System.Drawing.Font("Arial", 10F);
-            this.btnInvolement.Location = new System.Drawing.Point(15, 157);
-            this.btnInvolement.Name = "btnInvolement";
-            this.btnInvolement.Size = new System.Drawing.Size(92, 67);
-            this.btnInvolement.TabIndex = 8;
-            this.btnInvolement.Text = "\rInvolvement";
-            this.btnInvolement.UseVisualStyleBackColor = true;
-            // 
-            // lbxUsers
-            // 
-            this.lbxUsers.FormattingEnabled = true;
-            this.lbxUsers.Location = new System.Drawing.Point(734, 157);
-            this.lbxUsers.Name = "lbxUsers";
-            this.lbxUsers.Size = new System.Drawing.Size(120, 108);
-            this.lbxUsers.TabIndex = 9;
-            // 
-            // lbxUserAccounts
-            // 
-            this.lbxUserAccounts.FormattingEnabled = true;
-            this.lbxUserAccounts.Location = new System.Drawing.Point(734, 321);
-            this.lbxUserAccounts.Name = "lbxUserAccounts";
-            this.lbxUserAccounts.Size = new System.Drawing.Size(120, 199);
-            this.lbxUserAccounts.TabIndex = 10;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F);
-            this.label2.Location = new System.Drawing.Point(731, 136);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 18);
-            this.label2.TabIndex = 11;
-            this.label2.Text = "Users:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 12F);
-            this.label3.Location = new System.Drawing.Point(731, 300);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 18);
-            this.label3.TabIndex = 12;
-            this.label3.Text = "User accounts:";
-            // 
-            // btnAddAccount
-            // 
-            this.btnAddAccount.Location = new System.Drawing.Point(84, 150);
-            this.btnAddAccount.Name = "btnAddAccount";
-            this.btnAddAccount.Size = new System.Drawing.Size(223, 53);
-            this.btnAddAccount.TabIndex = 0;
-            this.btnAddAccount.Text = "Confirm opening new account";
-            this.btnAddAccount.UseVisualStyleBackColor = true;
+            this.tbxAddAccount.Font = new System.Drawing.Font("Arial", 12F);
+            this.tbxAddAccount.Location = new System.Drawing.Point(113, 107);
+            this.tbxAddAccount.Name = "tbxAddAccount";
+            this.tbxAddAccount.Size = new System.Drawing.Size(168, 26);
+            this.tbxAddAccount.TabIndex = 2;
             // 
             // label4
             // 
@@ -190,11 +100,148 @@
             this.label4.TabIndex = 1;
             this.label4.Text = "Open new account:";
             // 
+            // btnAddAccount
+            // 
+            this.btnAddAccount.Location = new System.Drawing.Point(84, 150);
+            this.btnAddAccount.Name = "btnAddAccount";
+            this.btnAddAccount.Size = new System.Drawing.Size(223, 53);
+            this.btnAddAccount.TabIndex = 0;
+            this.btnAddAccount.Text = "Confirm opening new account";
+            this.btnAddAccount.UseVisualStyleBackColor = true;
+            // 
+            // pnlCustomerButtons
+            // 
+            this.pnlCustomerButtons.Controls.Add(this.btnWithdrawCash);
+            this.pnlCustomerButtons.Controls.Add(this.btnBalance);
+            this.pnlCustomerButtons.Controls.Add(this.btnAddCash);
+            this.pnlCustomerButtons.Location = new System.Drawing.Point(358, 296);
+            this.pnlCustomerButtons.Name = "pnlCustomerButtons";
+            this.pnlCustomerButtons.Size = new System.Drawing.Size(301, 99);
+            this.pnlCustomerButtons.TabIndex = 3;
+            // 
+            // btnWithdrawCash
+            // 
+            this.btnWithdrawCash.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnWithdrawCash.Location = new System.Drawing.Point(203, 16);
+            this.btnWithdrawCash.Name = "btnWithdrawCash";
+            this.btnWithdrawCash.Size = new System.Drawing.Size(92, 67);
+            this.btnWithdrawCash.TabIndex = 6;
+            this.btnWithdrawCash.Text = "Withdraw cash";
+            this.btnWithdrawCash.UseVisualStyleBackColor = true;
+            // 
+            // btnBalance
+            // 
+            this.btnBalance.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnBalance.Location = new System.Drawing.Point(7, 16);
+            this.btnBalance.Name = "btnBalance";
+            this.btnBalance.Size = new System.Drawing.Size(92, 67);
+            this.btnBalance.TabIndex = 4;
+            this.btnBalance.Text = "Balance";
+            this.btnBalance.UseVisualStyleBackColor = true;
+            // 
+            // btnAddCash
+            // 
+            this.btnAddCash.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnAddCash.Location = new System.Drawing.Point(105, 16);
+            this.btnAddCash.Name = "btnAddCash";
+            this.btnAddCash.Size = new System.Drawing.Size(92, 67);
+            this.btnAddCash.TabIndex = 5;
+            this.btnAddCash.Text = "Add cash";
+            this.btnAddCash.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenAccount
+            // 
+            this.btnOpenAccount.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnOpenAccount.Location = new System.Drawing.Point(260, 312);
+            this.btnOpenAccount.Name = "btnOpenAccount";
+            this.btnOpenAccount.Size = new System.Drawing.Size(92, 67);
+            this.btnOpenAccount.TabIndex = 7;
+            this.btnOpenAccount.Text = "Open new account";
+            this.btnOpenAccount.UseVisualStyleBackColor = true;
+            // 
+            // btnInvolement
+            // 
+            this.btnInvolement.Font = new System.Drawing.Font("Arial", 10F);
+            this.btnInvolement.Location = new System.Drawing.Point(162, 312);
+            this.btnInvolement.Name = "btnInvolement";
+            this.btnInvolement.Size = new System.Drawing.Size(92, 67);
+            this.btnInvolement.TabIndex = 8;
+            this.btnInvolement.Text = "\rInvolvement";
+            this.btnInvolement.UseVisualStyleBackColor = true;
+            // 
+            // lbxUsers
+            // 
+            this.lbxUsers.Font = new System.Drawing.Font("Arial", 10F);
+            this.lbxUsers.FormattingEnabled = true;
+            this.lbxUsers.ItemHeight = 16;
+            this.lbxUsers.Location = new System.Drawing.Point(295, 126);
+            this.lbxUsers.Name = "lbxUsers";
+            this.lbxUsers.Size = new System.Drawing.Size(120, 164);
+            this.lbxUsers.TabIndex = 9;
+            this.lbxUsers.SelectedIndexChanged += new System.EventHandler(this.lbxUsers_SelectedIndexChanged);
+            // 
+            // lbxUserAccounts
+            // 
+            this.lbxUserAccounts.Font = new System.Drawing.Font("Arial", 10F);
+            this.lbxUserAccounts.FormattingEnabled = true;
+            this.lbxUserAccounts.ItemHeight = 16;
+            this.lbxUserAccounts.Location = new System.Drawing.Point(453, 126);
+            this.lbxUserAccounts.Name = "lbxUserAccounts";
+            this.lbxUserAccounts.Size = new System.Drawing.Size(120, 164);
+            this.lbxUserAccounts.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F);
+            this.label2.Location = new System.Drawing.Point(292, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Users:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F);
+            this.label3.Location = new System.Drawing.Point(450, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 18);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "User accounts:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F);
+            this.textBox1.Location = new System.Drawing.Point(247, 759);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 26);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F);
+            this.textBox2.Location = new System.Drawing.Point(247, 791);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial", 12F);
+            this.textBox3.Location = new System.Drawing.Point(247, 823);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 26);
+            this.textBox3.TabIndex = 15;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(891, 750);
+            this.ClientSize = new System.Drawing.Size(1235, 885);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbxUserAccounts);
@@ -232,6 +279,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnAddAccount;
+        private System.Windows.Forms.TextBox tbxAddAccount;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- OnlineBooking/OnlineBooking/Form1.cs
+++ OnlineBooking/OnlineBooking/Form1.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClassLibrary;
 
 namespace OnlineBooking
 {
     public partial class Form1 : Form
     {
+        System.Collections.ArrayList userList;
+        System.Collections.ArrayList accountList;
+        int userID = 0;
+
         public Form1()
         {
             InitializeComponent();
+
+            userList = new System.Collections.ArrayList();
+            accountList = new System.Collections.ArrayList();
+
+            userList.Add(new Customer() { Name = "Alex Bro", ID = 1 });
+            accountList.Add(new Account() { ID = 1 });
+            accountList.Add(new Account() { ID = 1 });
+            accountList.Add(new Account() { ID = 1 });
+            accountList.Add(new Account() { ID = 1 });
+            accountList.Add(new Account() { ID = 1 });
+
+            userList.Add(new Customer() { Name = "Urban Suedi", ID = 21 });
+
+
+            cbxSelectUser.Items.Add(new ComboBoxItem() { Text = "<Select option>", Value = 0 });
+            cbxSelectUser.Items.Add(new ComboBoxItem() { Text = "Customer", Value = 1 });
+            cbxSelectUser.Items.Add(new ComboBoxItem() { Text = "Employee", Value = 2 });
+
+            cbxSelectUser.SelectedIndex = 0;
+
+            CheckUsers();
+        }
+
+        private void cbxSelectUser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        void CheckUsers()
+        {
+            lbxUsers.Items.Clear();
+            foreach (Customer user in userList)
+            {
+                lbxUsers.Items.Add(user);
+            }
+        }
+
+        void CheckAccounts()
+        {
+            Customer customer = (Customer)lbxUsers.SelectedItem;
+            lbxUserAccounts.Items.Clear();
+            foreach (Account account in accountList)
+            {
+                if (customer.ID == account.ID)
+                {
+                    lbxUserAccounts.Items.Add(account);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void lbxUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Customer customer = (Customer)lbxUsers.SelectedItem;
+            CheckAccounts();
         }
     }
 }
