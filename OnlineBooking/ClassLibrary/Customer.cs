81cd969f4b20eda76520982c18926c780179017e
Felix Wetell <felix.wetell@gmail.com>
2016-09-22 14:48:30 +0200

    [OnlineBooking] Modified all the classes in 'Customer'

--- OnlineBooking/ClassLibrary/Customer.cs
+++ OnlineBooking/ClassLibrary/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +7,38 @@ using System.Threading.Tasks;
 
 namespace OnlineBooking
 {
-
     class Employee
     {
 
     }
 
-    class Customer
+    public class Customer
     {
-        //System.Collections.ArrayList Accounts;
+        Random r = new Random();
+        public string Name { get; set; }
+        public int ID { get; set; }
 
-        public string CustomerName { get; set; }
+        private ArrayList Accounts { get; set; }
+
+        public void CreateAccount()
+        {
+            Accounts.Add(new Account() { AccountName = "Konto", Balance = r.Next(1000, 5000), ID = 1 });
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 
-    class Account : Customer
+    public class Account
     {
         public string AccountName { get; set; }
-        public int AccountID { get; set; }
         public int Balance { get; set; }
+        public int ID { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Account{0}", ID);
+        }
     }
 }
