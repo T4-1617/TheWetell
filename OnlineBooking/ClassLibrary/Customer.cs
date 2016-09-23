39d0b22667cabf170749b68d571a7f09267c3696
Felix Wetell <felix.wetell@gmail.com>
2016-09-23 12:46:19 +0200

    [OnlineBooking] Removed private arraylist

--- OnlineBooking/ClassLibrary/Customer.cs
+++ OnlineBooking/ClassLibrary/Customer.cs
@@ -11,19 +11,23 @@ namespace OnlineBooking
     {
 
     }
-
     public class Customer
     {
         Random r = new Random();
+
         public string Name { get; set; }
         public int ID { get; set; }
 
-        private ArrayList Accounts { get; set; }
+        //private ArrayList Accounts { get; set; }
+        //public Customer()
+        //{
+        //    Accounts = new ArrayList();
+        //}
 
-        public void CreateAccount()
-        {
-            Accounts.Add(new Account() { AccountName = "Konto", Balance = r.Next(1000, 5000), ID = 1 });
-        }
+        //public void CreateAccount()
+        //{
+        //    Accounts.Add(new Account() { AccountName = "Account", Balance = r.Next(1000, 5000), ID = 11 });
+        //}
 
         public override string ToString()
         {
@@ -36,6 +40,14 @@ namespace OnlineBooking
         public string AccountName { get; set; }
         public int Balance { get; set; }
         public int ID { get; set; }
+
+        public Account()
+        { 
+            AccountName = string.Format("Account{0}", ID);
+            Balance = 1000;
+            ID = ID;
+        }
+
         public override string ToString()
         {
             return string.Format("Account{0}", ID);
