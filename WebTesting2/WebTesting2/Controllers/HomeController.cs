e16f8dc0697ac6aa5497a240edbf89621995e62c
Felix Wetell <felix.wetell@gmail.com>
2016-09-27 15:05:35 +0200

    Random web commit

---
+++ WebTesting2/WebTesting2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebTesting2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Text()
+        {
+            return "Hello! (from the webserver)";
+        }
+        //
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
